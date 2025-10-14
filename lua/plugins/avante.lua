62ed4ace5982637db85390f3df56e20312a842e0
9exa <resosudarmo.wisesa@gmail.com>
2025-10-15 03:57:20 +1100

    avante keymaps

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -15,26 +15,35 @@ return {
     -- for example
     provider = "copilot",
     -- provider = "claude",
-    -- providers = {
-    --   claude = {
-    --     endpoint = "https://api.anthropic.com",
-    --     model = "claude-sonnet-4-20250514",
-    --     timeout = 30000, -- Timeout in milliseconds
-    --       extra_request_body = {
-    --         temperature = 0.75,
-    --         max_tokens = 20480,
-    --       },
-    --   },
-    --   moonshot = {
-    --     endpoint = "https://api.moonshot.ai/v1",
-    --     model = "kimi-k2-0711-preview",
-    --     timeout = 30000, -- Timeout in milliseconds
-    --     extra_request_body = {
-    --       temperature = 0.75,
-    --       max_tokens = 32768,
-    --     },
-    --   },
-    -- },
+    providers = {
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-sonnet-4-20250514",
+        timeout = 30000, -- Timeout in milliseconds
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 20480,
+        },
+      },
+      moonshot = {
+        endpoint = "https://api.moonshot.ai/v1",
+        model = "kimi-k2-0711-preview",
+        timeout = 30000, -- Timeout in milliseconds
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 32768,
+        },
+      },
+    },
+    mappings = {
+      --- @class AvanteConflictMappings
+      suggestion = {
+        accept = "<A-l>",
+        next = "<A-]>",
+        prev = "<A-[>",
+        dismiss = "<C-]>",
+      },
+    },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
